bf43ded2c1e6722812deceaf48b138d7bc88ad9b
einardarri <git@einardarri.is>
2025-11-07 20:11:38 +0000

    stuff to install

--- bash/installs.sh
+++ bash/installs.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo apt update
+
 # install apt packages I want to have
-sudo apt install -y bind9-dnsutils 
+sudo apt install -y bind9-dnsutils ascii btop htop tree
 
 # batcat fany coloured cat in terminal
 sudo apt install -y bat
@@ -9,7 +11,7 @@ sudo apt install -y bat
 # mesurment tool for programing (line count and file size)
 sudo apt install -y cloc
 
-sudo apt install -y neofetch ascii
+sudo apt install -y neofetch 
 
 # install clang 
 sudo apt install -y clang build-essential libstdc++-14-dev
